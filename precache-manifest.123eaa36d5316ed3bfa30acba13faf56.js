13c9c41831bc06688f107bcc126dd7a6df206447
Kirsi84 <kirsi.rouvinen@edu.omnia.fi>
2020-05-29 18:42:18 +0300

    first commit

---
+++ precache-manifest.123eaa36d5316ed3bfa30acba13faf56.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "062679cac7f35a075ac987c1480a9f70",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c4b5a1b73a4ad61a0617",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "87571217c829ffce1c68",
+    "url": "/static/js/2.544e38ca.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.544e38ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4b5a1b73a4ad61a0617",
+    "url": "/static/js/main.b1ae1831.chunk.js"
+  },
+  {
+    "revision": "de4fc5150db60c7c2b08",
+    "url": "/static/js/runtime-main.62a6346a.js"
+  }
+]);
